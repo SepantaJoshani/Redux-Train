513bb639588c899bb2640515fde7508e793910f4
Sepanta <Sepanta_97@Yahoo.com>
2021-08-10 01:38:50 +0430

    working-with-slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
 import { createStore } from "redux";
+
 
 const initialState = {
   counter: 0,
   showCounter:true
 };
+
+createSlice({
+  name:"counter",
+  initialState,
+  reducers:{
+    increment(state){
+      state.counter++
+    },
+    decrement(state){
+      state.counter--
+    },
+    increase(state,action){
+      state.counter = state.counter + action.payload
+    },
+    toggleCounter(state){
+      state.showCounter = !state.showCounter
+    },
+  }
+})
 
 const counterReducer = (state = initialState, action) => {
 
