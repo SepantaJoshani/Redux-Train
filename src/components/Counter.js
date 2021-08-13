d47eebb487d9270f5abf2b4cf1c9c946920b113f
Sepanta <Sepanta_97@Yahoo.com>
2021-08-13 21:41:26 +0430

    display-with-useSelector

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+const counter = useSelector(state => state.counter)
+const dispatch = useDispatch()
+
   const toggleCounterHandler = () => {};
+  const incrementHandler = ()=>{
+    dispatch()
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>-- {counter} --</div>
+      <div>
+        <button>increment</button>
+        <button>decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
